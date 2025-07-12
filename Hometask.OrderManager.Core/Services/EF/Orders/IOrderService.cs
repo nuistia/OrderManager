48a46967635e7deac76596455b9dc2e96014f1a8
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-12 16:27:09 +0300

    Add order service and folder structure

---
+++ Hometask.OrderManager.Core/Services/EF/Orders/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using Hometask.OrderManager.Data.EF.Models;
+
+namespace Hometask.OrderManager.Core.Services.EF.Orders;
+
+public interface IOrderService
+{
+    void AddOrder(Order order);
+    void DeleteOrder(Order order);
+    void UpdateOrder(Order order);
+    List<Order> GetOrders();
+    Order GetOrderById(Guid id);
+    List<Order> GetLastYearOrders();
+}
