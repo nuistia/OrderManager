a7f8e94754705c3de96a4811ca3e6cb9f4301ff7
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-12 18:40:31 +0300

    Add all CRUD methods

--- Hometask.OrderManager.Data.ADO/Repositories/OrderRepository.cs
+++ Hometask.OrderManager.Data.ADO/Repositories/OrderRepository.cs
@@ -13,6 +13,115 @@ public class OrderRepository
         _connectionFactory = connectionFactory;
     }
 
+    public List<Order> GetOrdersLastYear()
+    {
+        return (List<Order>)GetOrdersLastYearWithSqlDataReader();
+    }
+
+    public List<Order> GetOrders()
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT OrdId, OrdDatetime, OrdAn FROM Orders";
+
+        using var reader = command.ExecuteReader();
+
+        var orders = new List<Order>();
+
+        while (reader.Read())
+        {
+            orders.Add(new Order
+            {
+                OrdId = reader.GetGuid(reader.GetOrdinal("OrdId")),
+                OrdDatetime = reader.IsDBNull(reader.GetOrdinal("OrdDatetime"))
+                    ? null
+                    : reader.GetDateTime(reader.GetOrdinal("OrdDatetime")),
+                OrdAn = reader.IsDBNull(reader.GetOrdinal("OrdAn"))
+                    ? null
+                    : reader.GetGuid(reader.GetOrdinal("OrdAn"))
+            });
+        }
+
+        return orders;
+    }
+
+    public Order? GetOrderById(Guid id)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "SELECT OrdId, OrdDatetime, OrdAn FROM Orders WHERE OrdId = @id";
+        command.Parameters.Add(new SqlParameter("@id", id));
+
+        using var reader = command.ExecuteReader();
+
+        if (reader.Read())
+        {
+            return new Order
+            {
+                OrdId = reader.GetGuid(reader.GetOrdinal("OrdId")),
+                OrdDatetime = reader.IsDBNull(reader.GetOrdinal("OrdDatetime"))
+                    ? null
+                    : reader.GetDateTime(reader.GetOrdinal("OrdDatetime")),
+                OrdAn = reader.IsDBNull(reader.GetOrdinal("OrdAn"))
+                    ? null
+                    : reader.GetGuid(reader.GetOrdinal("OrdAn"))
+            };
+        }
+
+        return null;
+    }
+
+    public void AddOrder(Order order)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = @"
+            INSERT INTO Orders (OrdId, OrdDatetime, OrdAn) 
+            VALUES (@id, @datetime, @ordan)";
+
+        command.Parameters.Add(new SqlParameter("@id", order.OrdId));
+        command.Parameters.Add(new SqlParameter("@datetime", (object?)order.OrdDatetime ?? DBNull.Value));
+        command.Parameters.Add(new SqlParameter("@ordan", (object?)order.OrdAn ?? DBNull.Value));
+
+        command.ExecuteNonQuery();
+    }
+
+    public void UpdateOrder(Order order)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = @"
+            UPDATE Orders 
+            SET OrdDatetime = @datetime, OrdAn = @ordan 
+            WHERE OrdId = @id";
+
+        command.Parameters.Add(new SqlParameter("@datetime", (object?)order.OrdDatetime ?? DBNull.Value));
+        command.Parameters.Add(new SqlParameter("@ordan", (object?)order.OrdAn ?? DBNull.Value));
+        command.Parameters.Add(new SqlParameter("@id", order.OrdId));
+
+        command.ExecuteNonQuery();
+    }
+
+    public void DeleteOrder(Order order)
+    {
+        using var connection = _connectionFactory.CreateConnection();
+        connection.Open();
+
+        using var command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM Orders WHERE OrdId = @id";
+        command.Parameters.Add(new SqlParameter("@id", order.OrdId));
+
+        command.ExecuteNonQuery();
+    }
+
     public IEnumerable<Order> GetOrdersLastYearWithSqlDataReader()
     {
         using var connection = _connectionFactory.CreateConnection();
