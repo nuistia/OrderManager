bd80f4886b7bfedc098142d012bdeb67ad5da8cf
Anastasiia Atanasevych <nuistia@gmail.com>
2025-07-12 17:34:57 +0300

    Add all settings files and DI

---
+++ Hometask.OrderManager.App/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using Hometask.OrderManager.Core.Services.EF.Orders;
+using Hometask.OrderManager.Data.EF;
+using Hometask.OrderManager.Data.EF.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Hometask.OrderManager.App;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<CHIITDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<IOrderService, OrderService>();
+
+        return services;
+    }
+
+    public static void ConfigureHost(IHostBuilder hostBuilder)
+    {
+        hostBuilder.ConfigureAppConfiguration((hostingContext, config) =>
+        {
+            var environment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+            config
+                .AddJsonFile(@"appsettings.json")
+                .AddJsonFile($@"appsettings.{environment}.json");
+        });
+
+        hostBuilder.ConfigureServices((context, services) =>
+        {
+            var configuration = context.Configuration;
+            services.RegisterServices(configuration);
+        });
+    }
+}
--- Hometask.OrderManager.App/Program.cs
+++ Hometask.OrderManager.App/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace Hometask.OrderManager.App;
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Hometask.OrderManager.App;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        
+        HostBuilder hostBuilder = new();
+
+        DependencyInjection.ConfigureHost(hostBuilder);
+
+        using IHost host = hostBuilder.Build();
     }
 }
